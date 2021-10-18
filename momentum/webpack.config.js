6cb48441edc28349f5872a933d4981b30d1b01dd
PANTORY <pantory@gmail.com>
2021-10-18 17:49:10 +0300

    init: start momentum task

---
+++ momentum/webpack.config.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin');
+
+const devServer = (isDev) => !isDev ? {} : {
+  devServer: {
+    open: true,
+    port: 8080,
+    contentBase: './src'
+  },
+};
+
+module.exports = ({ development }) => ({
+  mode: development ? 'development' : 'production',
+  devtool: development ? 'inline-source-map' : false,
+  entry: {
+    index: './src/scripts/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/images/[name][ext]'
+  },
+  optimization: {
+    minimize: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/svg/[name][ext]'
+        },
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name][ext]'
+        },
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader'
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'style.css' }),
+    new HtmlWebpackPlugin({
+      template: 'src/pages/index.pug',
+      favicon: './src/favicon.ico',
+      filename: 'index.html'
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/assets',
+          to: 'assets'
+        }
+      ],
+    }),
+    new ImageminWebpWebpackPlugin()
+  ],
+  resolve: {
+    extensions: ['.js'],
+  },
+  ...devServer(development)
+});
