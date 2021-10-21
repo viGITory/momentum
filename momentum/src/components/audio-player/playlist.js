80537655d5c572573b8efb0c238d5cb53716d613
PANTORY <pantory@gmail.com>
2021-10-21 19:22:50 +0300

    refactor: replace playlist animation/minor fixes

--- momentum/src/components/audio-player/playlist.js
+++ momentum/src/components/audio-player/playlist.js
@@ -1,17 +1,17 @@
 const trackList = [
   {
     title: 'Rearrange My Mind - Monolink',
-    src: '../assets/sounds/Rearrange My Mind - Monolink.mp3',
+    src: './assets/sounds/Rearrange My Mind - Monolink.mp3',
     duration: '01:10'
   },
   {
     title: 'Neptune - Beauvois',
-    src: '../assets/sounds/Neptune - Beauvois.mp3',
+    src: './assets/sounds/Neptune - Beauvois.mp3',
     duration: '01:43'
   },
   {
     title: 'Lilith - AUGUSTA TREVERORUM',
-    src: '../assets/sounds/Lilith - AUGUSTA TREVERORUM.mp3',
+    src: './assets/sounds/Lilith - AUGUSTA TREVERORUM.mp3',
     duration: '01:12'
   }
 ];
