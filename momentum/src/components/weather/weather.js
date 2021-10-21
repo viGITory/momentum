ba33c42fb5d3de9d9552f3d7696531077d266517
PANTORY <pantory@gmail.com>
2021-10-22 00:03:40 +0300

    feat: add warning msg for incorrect city in weather widget

--- momentum/src/components/weather/weather.js
+++ momentum/src/components/weather/weather.js
@@ -10,21 +10,36 @@ async function getWeather() {
   let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${weatherCity.value}&lang=en&appid=a8122fbe52b443584fbcba6f23095ca1&units=metric`;
 
   const weatherRes = await fetch(weatherUrl);
-  const weatherData = await weatherRes.json();
 
-  weatherIcon.className = 'weather__icon owf';
-  weatherIcon.classList.add(`owf-${weatherData.weather[0].id}`);
+  if (weatherRes.status === 200) {
+    const weatherData = await weatherRes.json();
 
-  weatherTemp.textContent = `${weatherData.main.temp.toFixed(0)}°C`;
-  weatherDescr.textContent = `${weatherData.weather[0].description[0].toUpperCase()}${weatherData.weather[0].description.slice(1)}`;
-  weatherWind.textContent = `Wind speed: ${weatherData.wind.speed.toFixed(0)}m/s`;
-  weatherHumidity.textContent = `Humidity: ${weatherData.main.humidity}%`;
+    weatherIcon.className = 'weather__icon owf';
+    weatherIcon.classList.add(`owf-${weatherData.weather[0].id}`);
+
+    weatherTemp.textContent = `${weatherData.main.temp.toFixed(0)}°C`;
+    weatherDescr.textContent = `${weatherData.weather[0].description[0].toUpperCase()}${weatherData.weather[0].description.slice(1)}`;
+    weatherWind.textContent = `Wind speed: ${weatherData.wind.speed.toFixed(0)}m/s`;
+    weatherHumidity.textContent = `Humidity: ${weatherData.main.humidity}%`;
+
+    weatherCity.placeholder = '[Enter city]';
+    weatherCity.classList.remove('js-weather-warn');
+  } else {
+    weatherCity.value = '';
+    weatherCity.placeholder = 'City not found';
+    weatherCity.classList.add('js-weather-warn');
+  }
 }
 
 document.addEventListener('DOMContentLoaded', getWeather);
 weatherCity.addEventListener('change', () => {
-  getWeather();
-  weatherCity.value = `${weatherCity.value[0].toUpperCase()}${weatherCity.value.slice(1)}`;
+  if (weatherCity.value) {
+    getWeather();
+    weatherCity.value = `${weatherCity.value[0].toUpperCase()}${weatherCity.value.slice(1)}`;
+  } else {
+    weatherCity.placeholder = 'City not found';
+    weatherCity.classList.add('js-weather-warn');
+  }
 });
 setInterval(getWeather, 600000);
 
