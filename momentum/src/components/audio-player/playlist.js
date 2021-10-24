1c5ff16c12bc1f7ec4debe50066ac1388099f46d
PANTORY <pantory@gmail.com>
2021-10-25 01:10:21 +0300

    refactor: replace audio tracks

--- momentum/src/components/audio-player/playlist.js
+++ momentum/src/components/audio-player/playlist.js
@@ -1,18 +1,18 @@
 const trackList = [
   {
-    title: 'Rearrange My Mind - Monolink',
-    src: './assets/sounds/Rearrange My Mind - Monolink.mp3',
-    duration: '01:10'
+    title: 'Ott - Cley Hill',
+    src: './assets/sounds/Ott - Cley Hill.mp3',
+    duration: '01:15'
   },
   {
-    title: 'Neptune - Beauvois',
-    src: './assets/sounds/Neptune - Beauvois.mp3',
-    duration: '01:43'
+    title: 'Ott - Roflcopter',
+    src: './assets/sounds/Ott - Roflcopter.mp3',
+    duration: '00:56'
   },
   {
-    title: 'Lilith - AUGUSTA TREVERORUM',
-    src: './assets/sounds/Lilith - AUGUSTA TREVERORUM.mp3',
-    duration: '01:12'
+    title: 'Ott - Escape From Tulse Hell',
+    src: './assets/sounds/Ott - Escape From Tulse Hell.mp3',
+    duration: '01:25'
   }
 ];
 export default trackList;
