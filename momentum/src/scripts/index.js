12d8bed266a3d9abecd514653030e8de3f05de91
PANTORY <pantory@gmail.com>
2021-10-26 02:20:12 +0300

    refactor: add self-rate

--- momentum/src/scripts/index.js
+++ momentum/src/scripts/index.js
@@ -6,3 +6,6 @@ import '../components/audio-player/audio-player';
 import '../components/date/date';
 import '../components/quotes/quotes';
 import '../components/weather/weather';
+
+// *** SELF-RATE ***
+import './self-rate';
