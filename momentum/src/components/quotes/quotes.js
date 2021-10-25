0478206587e458ab6ee431439cd120a86524f1a2
PANTORY <pantory@gmail.com>
2021-10-26 02:02:52 +0300

    refactor: replace quotes api

--- momentum/src/components/quotes/quotes.js
+++ momentum/src/components/quotes/quotes.js
@@ -1,31 +1,20 @@
 const quoteBtn = document.querySelector('.quotes__button');
 const quoteText = document.querySelector('.quotes__text');
 const quoteAuthor = document.querySelector('.quotes__author');
-
-const randomize = (arr) => {
-  for (let i = arr.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-
-    [arr[i], arr[j]] = [arr[j], arr[i]];
-  }
-};
+let id;
 
 async function getQuotes() {
-  const quotes = 'https://www.breakingbadapi.com/api/quotes';
-  const res = await fetch(quotes);
+  const quote = `https://genius-quotes.herokuapp.com/api/quotes/random?prev=${id}`;
+  const res = await fetch(quote);
   const data = await res.json();
-  const quotesArr = [];
 
-  data.forEach(item => quotesArr.push(item));
+  id = data.id;
 
-  randomize(quotesArr);
-
-  quoteText.textContent = `"${quotesArr[0].quote}"`;
-  quoteAuthor.textContent = quotesArr[0].author;
+  quoteText.textContent = `"${data.text}"`;
+  quoteAuthor.textContent = data.author;
 }
 getQuotes();
 
-document.addEventListener('onload', getQuotes);
 quoteBtn.addEventListener('click', () => {
   quoteBtn.classList.add('js-rotate-btn');
   getQuotes();
