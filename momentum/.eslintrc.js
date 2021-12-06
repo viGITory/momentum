e831eb8bbe23bb5442226a31cae34745b36d7fbb
PANTORY <pantory@gmail.com>
2021-12-06 16:35:58 +0300

    refactor: add prettier

--- momentum/.eslintrc.js
+++ momentum/.eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2020": true
-    },
-    "extends": "airbnb-base",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': ['error'],
+  },
 };
