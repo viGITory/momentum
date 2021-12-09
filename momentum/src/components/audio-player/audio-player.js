29babf0c284d35aeb313edd9cb129f136d9a10c4
PANTORY <pantory@gmail.com>
2021-12-09 18:45:20 +0300

    refactor: rewrite audio-player module to js class

--- momentum/src/components/audio-player/audio-player.js
+++ momentum/src/components/audio-player/audio-player.js
@@ -1,167 +1,193 @@
-import trackList from './playlist';
+import playlist from './playlist';
 
-const playBtn = document.querySelector('.audio-player__button--play');
-const prevBtn = document.querySelector('.audio-player__button--prev');
-const nextBtn = document.querySelector('.audio-player__button--next');
-const volumeBtn = document.querySelector('.audio-player__button--volume');
-const progressBar = document.querySelector('.audio-player__progress');
-const progressTrackName = document.querySelector('.audio-player__name');
-const progressTime = document.querySelector('.audio-player__time');
-const audioDuration = document.querySelector('.audio-player__duration');
+export default class AudioPlayer {
+  constructor() {
+    this.playButton = document.querySelector('.audio-player__button--play');
+    this.prevButton = document.querySelector('.audio-player__button--prev');
+    this.nextButton = document.querySelector('.audio-player__button--next');
+    this.volumeButton = document.querySelector('.audio-player__button--volume');
+    this.progressBar = document.querySelector('.audio-player__progress');
+    this.progressTrackName = document.querySelector('.audio-player__name');
+    this.progressTime = document.querySelector('.audio-player__time');
+    this.audioDuration = document.querySelector('.audio-player__duration');
+    this.volumeBar = document.querySelector('.audio-player__volume-bar');
+    this.playerPlaylist = document.querySelector('.audio-player__playlist');
 
-const playlist = document.querySelector('.audio-player__playlist');
-const audioPlayer = new Audio();
-let index = 0;
+    this.audioPlayer = new Audio();
+    this.audioPlayer.volume = 0.5;
+    this.currentVolume = this.audioPlayer.volume;
 
-trackList.forEach((item) => {
-  const track = document.createElement('li');
-
-  track.classList.add('audio-player__track');
-  audioPlayer.src = trackList[index].src;
-  audioDuration.textContent = trackList[index].duration;
-  progressTrackName.textContent = trackList[index].title;
-
-  playlist.append(track);
-  track.textContent = item.title;
-});
-
-const trackArr = document.querySelectorAll('.audio-player__track');
-
-const playAudio = () => {
-  audioDuration.textContent = trackList[index].duration;
-  progressTrackName.textContent = trackList[index].title;
-
-  if (audioPlayer.paused) {
-    trackArr[index].classList.add('js-track-play');
-    playBtn.classList.add('js-play-btn');
-    audioPlayer.play();
-  } else {
-    audioPlayer.pause();
-    trackArr[index].classList.remove('js-track-play');
-    playBtn.classList.remove('js-play-btn');
-  }
-};
-
-audioPlayer.addEventListener('ended', () => {
-  if (index === trackList.length - 1) {
-    index = 0;
-  } else {
-    index += 1;
+    this.currentTrack = 0;
   }
 
-  trackArr.forEach((item) => item.classList.remove('js-track-play'));
+  createPlaylist() {
+    playlist.forEach((item) => {
+      const track = document.createElement('li');
 
-  audioPlayer.src = trackList[index].src;
-  playAudio();
-});
-playBtn.addEventListener('click', () => {
-  playAudio();
-});
-prevBtn.addEventListener('click', () => {
-  if (index <= 0) {
-    index = trackList.length - 1;
-  } else {
-    index -= 1;
+      track.classList.add('audio-player__track');
+      this.audioPlayer.src = playlist[this.currentTrack].src;
+      this.audioDuration.textContent = playlist[this.currentTrack].duration;
+      this.progressTrackName.textContent = playlist[this.currentTrack].title;
+
+      this.playerPlaylist.append(track);
+      track.textContent = item.title;
+    });
   }
 
-  audioPlayer.src = trackList[index].src;
+  playAudio() {
+    const tracks = document.querySelectorAll('.audio-player__track');
 
-  trackArr.forEach((item) => item.classList.remove('js-track-play'));
-  playAudio();
-});
-nextBtn.addEventListener('click', () => {
-  if (index === trackList.length - 1) {
-    index = 0;
-  } else {
-    index += 1;
+    this.audioDuration.textContent = playlist[this.currentTrack].duration;
+    this.progressTrackName.textContent = playlist[this.currentTrack].title;
+
+    if (this.audioPlayer.paused) {
+      tracks[this.currentTrack].classList.add('js-track-play');
+      this.playButton.classList.add('js-play-btn');
+      this.audioPlayer.play();
+    } else {
+      this.audioPlayer.pause();
+      tracks[this.currentTrack].classList.remove('js-track-play');
+      this.playButton.classList.remove('js-play-btn');
+    }
   }
 
-  audioPlayer.src = trackList[index].src;
+  showAudioProgress = () => {
+    const currentMinutes =
+      Math.floor(this.audioPlayer.currentTime / 60) < 10
+        ? `0${Math.floor(this.audioPlayer.currentTime / 60)}`
+        : Math.floor(this.audioPlayer.currentTime / 60);
 
-  trackArr.forEach((item) => item.classList.remove('js-track-play'));
-  playAudio();
-});
+    const currentSeconds =
+      Math.floor(this.audioPlayer.currentTime % 60) < 10
+        ? `0${Math.floor(this.audioPlayer.currentTime % 60)}`
+        : Math.floor(this.audioPlayer.currentTime % 60);
 
-// *** PROGRESS ***
-const audioProgress = () => {
-  const currentMinutes =
-    Math.floor(audioPlayer.currentTime / 60) < 10
-      ? `0${Math.floor(audioPlayer.currentTime / 60)}`
-      : Math.floor(audioPlayer.currentTime / 60);
-  const currentSeconds =
-    Math.floor(audioPlayer.currentTime % 60) < 10
-      ? `0${Math.floor(audioPlayer.currentTime % 60)}`
-      : Math.floor(audioPlayer.currentTime % 60);
+    this.progressTime.textContent = `${currentMinutes}:${currentSeconds}`;
+    this.progressBar.value =
+      Math.floor(this.audioPlayer.currentTime) /
+        (Math.floor(this.audioPlayer.duration) / 100) || 0;
+    this.progressBar.style.background = `linear-gradient(to right, #ff4040 0%, #ff4040 ${this.progressBar.value}%, #fff ${this.progressBar.value}%, #fff 100%)`;
+  };
 
-  progressTime.textContent = `${currentMinutes}:${currentSeconds}`;
-  progressBar.value =
-    Math.floor(audioPlayer.currentTime) /
-      (Math.floor(audioPlayer.duration) / 100) || 0;
-  progressBar.style.background = `linear-gradient(to right, #ff4040 0%, #ff4040 ${progressBar.value}%, #fff ${progressBar.value}%, #fff 100%)`;
-};
-
-const changeAudioTime = () => {
-  audioPlayer.currentTime = audioPlayer.duration * (progressBar.value / 100);
-};
-
-audioPlayer.addEventListener('timeupdate', audioProgress);
-progressBar.addEventListener('pointerdown', () => {
-  audioPlayer.removeEventListener('timeupdate', audioProgress);
-  progressBar.addEventListener('input', () => {
-    progressBar.style.background = `linear-gradient(to right, #ff4040 0%, #ff4040 ${progressBar.value}%,  #fff ${progressBar.value}%, #fff 100%)`;
-  });
-
-  progressBar.addEventListener('pointerup', (event) => {
-    changeAudioTime(event);
-    audioPlayer.addEventListener('timeupdate', audioProgress);
-  });
-
-  progressBar.addEventListener('touchend', () => {
-    changeAudioTime();
-    audioPlayer.addEventListener('timeupdate', audioProgress);
-  });
-});
-
-// *** VOLUME ***
-const volumeBar = document.querySelector('.audio-player__volume-bar');
-audioPlayer.volume = 0.5;
-let currentVolume = audioPlayer.volume;
-
-const сhangeVolume = () => {
-  const volume = volumeBar.value / 100;
-  audioPlayer.volume = volume;
-
-  if (audioPlayer.volume !== 0) currentVolume = audioPlayer.volume;
-
-  volumeBar.style.background = `linear-gradient(to right, #ff4040 ${
-    volume * 100
-  }%, #ff4040 ${volume * 100}%,  #fff ${volume * 100}%, #fff 100%)`;
-
-  if (audioPlayer.volume === 0) {
-    volumeBtn.style.backgroundImage = 'url(assets/svg/mute.svg)';
-  } else {
-    volumeBtn.style.backgroundImage = 'url(assets/svg/volume-btn.svg)';
+  changeAudioTime() {
+    this.audioPlayer.currentTime =
+      this.audioPlayer.duration * (this.progressBar.value / 100);
   }
-};
 
-const muteVolume = () => {
-  if (audioPlayer.volume === 0) {
-    audioPlayer.volume = currentVolume;
-    volumeBar.value = audioPlayer.volume * 100;
-    volumeBtn.style.backgroundImage = 'url(assets/svg/volume-btn.svg)';
-    volumeBar.style.background = `linear-gradient(to right, #ff4040 ${
-      currentVolume * 100
-    }%, #ff4040 ${currentVolume * 100}%,  #fff ${
-      currentVolume * 100
-    }%, #fff 100%)`;
-  } else {
-    audioPlayer.volume = 0;
-    volumeBar.value = 0;
-    volumeBtn.style.backgroundImage = 'url(assets/svg/mute.svg)';
-    volumeBar.style.background = `linear-gradient(to right,  #ff4040 0%, #ff4040 0%, #fff 0%, #fff 100%)`;
+  сhangeVolume = () => {
+    const volume = this.volumeBar.value / 100;
+    this.audioPlayer.volume = volume;
+
+    if (this.audioPlayer.volume !== 0) {
+      this.currentVolume = this.audioPlayer.volume;
+    }
+
+    if (this.audioPlayer.volume === 0) {
+      this.volumeButton.style.backgroundImage = 'url(assets/svg/mute.svg)';
+    } else {
+      this.volumeButton.style.backgroundImage =
+        'url(assets/svg/volume-btn.svg)';
+    }
+
+    this.volumeBar.style.background = `linear-gradient(to right, #ff4040 ${
+      volume * 100
+    }%, #ff4040 ${volume * 100}%,  #fff ${volume * 100}%, #fff 100%)`;
+  };
+
+  muteVolume = () => {
+    if (this.audioPlayer.volume === 0) {
+      this.audioPlayer.volume = this.currentVolume;
+      this.volumeBar.value = this.audioPlayer.volume * 100;
+      this.volumeButton.style.backgroundImage =
+        'url(assets/svg/volume-btn.svg)';
+      this.volumeBar.style.background = `linear-gradient(to right, #ff4040 ${
+        this.currentVolume * 100
+      }%, #ff4040 ${this.currentVolume * 100}%,  #fff ${
+        this.currentVolume * 100
+      }%, #fff 100%)`;
+    } else {
+      this.audioPlayer.volume = 0;
+      this.volumeBar.value = 0;
+      this.volumeButton.style.backgroundImage = 'url(assets/svg/mute.svg)';
+      this.volumeBar.style.background = `linear-gradient(to right,  #ff4040 0%, #ff4040 0%, #fff 0%, #fff 100%)`;
+    }
+  };
+
+  addListeners() {
+    const tracks = document.querySelectorAll('.audio-player__track');
+
+    this.playButton.addEventListener('click', () => {
+      this.playAudio();
+    });
+
+    this.prevButton.addEventListener('click', () => {
+      if (this.currentTrack <= 0) {
+        this.currentTrack = playlist.length - 1;
+      } else {
+        this.currentTrack -= 1;
+      }
+
+      this.audioPlayer.src = playlist[this.currentTrack].src;
+
+      tracks.forEach((item) => item.classList.remove('js-track-play'));
+      this.playAudio();
+    });
+
+    this.nextButton.addEventListener('click', () => {
+      if (this.currentTrack === playlist.length - 1) {
+        this.currentTrack = 0;
+      } else {
+        this.currentTrack += 1;
+      }
+
+      this.audioPlayer.src = playlist[this.currentTrack].src;
+
+      tracks.forEach((item) => item.classList.remove('js-track-play'));
+      this.playAudio();
+    });
+
+    this.audioPlayer.addEventListener('ended', () => {
+      if (this.currentTrack === playlist.length - 1) {
+        this.currentTrack = 0;
+      } else {
+        this.currentTrack += 1;
+      }
+
+      tracks.forEach((item) => item.classList.remove('js-track-play'));
+
+      this.audioPlayer.src = playlist[this.currentTrack].src;
+      this.playAudio();
+    });
+
+    this.audioPlayer.addEventListener('timeupdate', this.showAudioProgress);
+
+    this.progressBar.addEventListener('pointerdown', () => {
+      this.audioPlayer.removeEventListener(
+        'timeupdate',
+        this.showAudioProgress
+      );
+      this.progressBar.addEventListener('input', () => {
+        this.progressBar.style.background = `linear-gradient(to right, #ff4040 0%, #ff4040 ${this.progressBar.value}%,  #fff ${this.progressBar.value}%, #fff 100%)`;
+      });
+
+      this.progressBar.addEventListener('pointerup', (event) => {
+        this.changeAudioTime(event);
+        this.audioPlayer.addEventListener('timeupdate', this.showAudioProgress);
+      });
+
+      this.progressBar.addEventListener('touchend', () => {
+        this.changeAudioTime();
+        this.audioPlayer.addEventListener('timeupdate', this.showAudioProgress);
+      });
+    });
+
+    this.volumeButton.addEventListener('click', this.muteVolume);
+    this.volumeBar.addEventListener('change', this.сhangeVolume);
+    this.volumeBar.addEventListener('input', this.сhangeVolume);
   }
-};
 
-volumeBtn.addEventListener('click', muteVolume);
-volumeBar.addEventListener('change', сhangeVolume);
-volumeBar.addEventListener('input', сhangeVolume);
+  init() {
+    this.createPlaylist();
+    this.addListeners();
+  }
+}
--- momentum/src/scripts/index.js
+++ momentum/src/scripts/index.js
@@ -2,18 +2,15 @@ import '../styles/style.scss';
 
 // *** COMPONENTS ***
 import ImageSlider from '../components/page/image-slider';
-import '../components/audio-player/audio-player';
+import AudioPlayer from '../components/audio-player/audio-player';
 import DateWidget from '../components/date/date-widget';
 import Quotes from '../components/quotes/quotes';
 import '../components/weather/weather';
-
-// *** API ***
-import '../api/weather-api';
-import '../api/quotes-api';
 
 // *** SELF-RATE ***
 import './self-rate';
 
 new DateWidget().init();
 new Quotes().init();
+new AudioPlayer().init();
 new ImageSlider().init();
