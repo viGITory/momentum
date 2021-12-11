b1186327af54db2f98e1329092c0aff52e22d314
PANTORY <pantory@gmail.com>
2021-12-11 23:48:21 +0300

    refactor: replace images api

--- momentum/src/api/image-api.js
+++ momentum/src/api/image-api.js
@@ -1,6 +1,6 @@
 export default async function getImage(timeOfDay, randomNum) {
   const result = await fetch(
-    `https://raw.githubusercontent.com/viGITory/stage1-tasks/assets/images/${timeOfDay}/${randomNum}.jpg`
+    `https://raw.githubusercontent.com/viGITory/momentum-images/main/${timeOfDay}/${randomNum}.jpg`
   );
 
   const blob = await result.blob();
