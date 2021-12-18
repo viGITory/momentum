a0621a41d6c1399cb668bfeb0b17a2b1ba5f3e62
PANTORY <pantory@gmail.com>
2021-12-18 15:47:43 +0300

    fix: save city to localstorage only on valid request

--- momentum/src/components/weather/weather.js
+++ momentum/src/components/weather/weather.js
@@ -13,15 +13,23 @@ export default class Weather {
     this.weatherHumidity = document.querySelector('.weather__humidity');
 
     this.warnMessage = document.createElement('span');
+
+    this.weatherCity.value = localStorage.getItem('vigitory-city') || 'Minsk';
   }
 
   async setWeather() {
     try {
       this.dataWrapper.classList.remove('js-show-elem');
 
-      if (this.warnMessage) this.warnMessage.remove();
-
       const weatherData = await getWeather(this.weatherCity.value);
+
+      if (this.warnMessage) this.warnMessage.remove();
+      if (weatherData.cod === 200) {
+        this.weatherCity.value = `${this.weatherCity.value[0].toUpperCase()}${this.weatherCity.value.slice(
+          1
+        )}`;
+        localStorage.setItem('vigitory-city', this.weatherCity.value);
+      }
 
       this.dataWrapper.classList.add('js-show-elem');
       this.weatherIcon.className = 'weather__icon owf';
@@ -44,9 +52,6 @@ export default class Weather {
       }deg)`;
       this.windDirection.style.backgroundImage =
         'url("../assets/svg/wind-arrow.svg")';
-      this.weatherCity.value = `${this.weatherCity.value[0].toUpperCase()}${this.weatherCity.value.slice(
-        1
-      )}`;
     } catch (err) {
       this.weatherCity.value = '';
 
@@ -71,23 +76,10 @@ export default class Weather {
         this.setWeather();
       }
     });
-
-    window.addEventListener('beforeunload', () => {
-      localStorage.setItem('vigitory-city', this.weatherCity.value);
-    });
-
-    document.addEventListener('DOMContentLoaded', () => {
-      if (localStorage.getItem('vigitory-city')) {
-        this.weatherCity.value = localStorage.getItem('vigitory-city');
-      } else {
-        this.weatherCity.value = 'Minsk';
-      }
-
-      this.setWeather();
-    });
   }
 
   init() {
+    this.setWeather();
     this.addListeners();
 
     setInterval(() => this.setWeather(), 600000);
