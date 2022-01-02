321fe2879088b3c4f333abbb0b23f52cc231034d
PANTORY <pantory@gmail.com>
2022-01-03 00:43:13 +0300

    refactor: add noopener noreferrer to links

--- momentum/src/components/main-footer/main-footer.js
+++ momentum/src/components/main-footer/main-footer.js
@@ -8,10 +8,10 @@ export default class Footer {
   render() {
     this.container.innerHTML = `
       <div class="main-footer__left">
-        <a class="main-footer__link" href="https://github.com/viGITory" target="_blank" title="“This is the way..” (The Mandalorian)">viGITory</a>
+        <a class="main-footer__link" href="https://github.com/viGITory" target="_blank" rel="noopener noreferrer" title="“This is the way..” (The Mandalorian)">viGITory</a>
         <time datetime="2021">2021</time>
       </div>
-      <a class="main-footer__logo" href="https://rs.school/js" target="_blank">
+      <a class="main-footer__logo" href="https://rs.school/js" target="_blank" rel="noopener noreferrer">
         <span class="visually-hidden">Rsschool</span>
       </a>
     `;
