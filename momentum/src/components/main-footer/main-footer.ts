76c633714c1e99aac7b281f590724078d154581d
PANTORY <pantory@gmail.com>
2022-01-19 00:55:44 +0300

    refactor: migrate MainFooter component to TS

--- momentum/src/components/main-footer/main-footer.ts
+++ momentum/src/components/main-footer/main-footer.ts
@@ -1,11 +1,13 @@
 export default class MainFooter {
+  container: HTMLElement;
+
   constructor() {
-    this.container = document.createElement('footer');
+    this.container = document.createElement('footer') as HTMLElement;
     this.container.classList.add('section', 'main-footer');
     this.container.id = 'section-footer';
   }
 
-  render() {
+  render(): HTMLElement {
     this.container.innerHTML = `
       <div class="main-footer__left">
         <a class="main-footer__link" href="https://github.com/viGITory" target="_blank" rel="noopener noreferrer" title="“This is the way..” (The Mandalorian)">viGITory</a>
