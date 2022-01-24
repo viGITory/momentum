996f1bb06359bdd27b81409bcaf181a68f35c5f0
PANTORY <pantory@gmail.com>
2022-01-24 16:13:21 +0300

    refactor: migrate TodoList component to TS

--- momentum/src/components/todo-list/todo-list.ts
+++ momentum/src/components/todo-list/todo-list.ts
@@ -1,15 +1,22 @@
 import removeTodoItem from '../../utils/remove-todo-item';
 
 export default class TodoList {
+  container: HTMLElement;
+  newTodoItem!: HTMLLIElement;
+  newTodoInput!: HTMLInputElement;
+  todoList!: HTMLUListElement;
+
+  todoStorage: string[];
+
   constructor() {
-    this.container = document.createElement('section');
+    this.container = document.createElement('section') as HTMLElement;
     this.container.classList.add('page__todo-list', 'section', 'todo-list');
     this.container.id = 'section-todo';
 
     this.todoStorage = [];
   }
 
-  render() {
+  render(): HTMLElement {
     this.container.innerHTML = `
       <h2 class="visually-hidden">Todo</h2>
       <div class="todo-list__top">
@@ -21,8 +28,8 @@ export default class TodoList {
     return this.container;
   }
 
-  createTodoItem(value) {
-    this.newTodoItem = document.createElement('li');
+  createTodoItem(value: string): HTMLLIElement {
+    this.newTodoItem = document.createElement('li') as HTMLLIElement;
     this.newTodoItem.classList.add('todo-list__item');
 
     this.newTodoItem.innerHTML = `
@@ -40,48 +47,56 @@ export default class TodoList {
     return this.newTodoItem;
   }
 
-  getElements() {
-    this.newTodoInput = this.container.querySelector('[name=new-todo-input]');
-    this.todoList = this.container.querySelector('.todo-list__list');
+  getElements(): void {
+    this.newTodoInput = this.container.querySelector(
+      '[name=new-todo-input]'
+    ) as HTMLInputElement;
+    this.todoList = this.container.querySelector(
+      '.todo-list__list'
+    ) as HTMLUListElement;
   }
 
-  addListeners() {
+  addListeners(): void {
     this.newTodoInput.addEventListener('change', () => {
       this.todoList.append(this.createTodoItem(this.newTodoInput.value));
     });
 
     this.todoList.addEventListener('click', (event) => {
-      const { target } = event;
-      const targetParent = target.parentNode;
+      const target = event.target as HTMLButtonElement;
+      const targetParent = target.parentNode as HTMLDivElement;
 
       if (target.name === 'button-complete') {
         target.classList.add('js-active-btn');
 
         target.style.color = '#ff4040';
-        targetParent.parentNode
-          .querySelector('.todo-list__input-wrapper')
+        targetParent
+          .parentNode!.querySelector('.todo-list__input-wrapper')!
           .classList.add('todo-list__input-wrapper--complete');
-        targetParent.parentNode.querySelector(
+        targetParent.parentNode!.querySelector<HTMLInputElement>(
           '.todo-list__input'
-        ).readOnly = true;
-        targetParent.parentNode.querySelector('.todo-list__input').style.color =
-          'rgba(255, 255, 255, 0.5)';
+        )!.readOnly = true;
+        targetParent.parentNode!.querySelector<HTMLInputElement>(
+          '.todo-list__input'
+        )!.style.color = 'rgba(255, 255, 255, 0.5)';
       } else if (target.name === 'button-delete') {
         removeTodoItem(event);
       }
     });
 
     this.todoList.addEventListener('change', (event) => {
-      const { target } = event;
-      const targetParent = target.parentNode;
+      const target = event.target as HTMLInputElement;
+      const targetParent = target.parentNode as HTMLDivElement;
 
-      if (targetParent.querySelector('.todo-list__input').value === '') {
+      if (
+        targetParent.querySelector<HTMLInputElement>('.todo-list__input')!
+          .value === ''
+      ) {
         removeTodoItem(event);
       }
     });
 
     window.addEventListener('beforeunload', () => {
-      const todoItems = this.container.querySelectorAll(
+      const todoItems = this.container.querySelectorAll<HTMLInputElement>(
         '.todo-list__input-wrapper > .todo-list__input'
       );
 
@@ -98,17 +113,19 @@ export default class TodoList {
     });
 
     document.addEventListener('DOMContentLoaded', () => {
-      const todoItems = JSON.parse(localStorage.getItem('vigitory-todo-list'));
+      const todoItems = JSON.parse(
+        localStorage.getItem('vigitory-todo-list') || ''
+      );
 
       if (todoItems) {
-        todoItems.forEach((item) => {
+        todoItems.forEach((item: string) => {
           this.todoList.append(this.createTodoItem(item));
         });
       }
     });
   }
 
-  init() {
+  init(): void {
     this.getElements();
     this.addListeners();
   }
