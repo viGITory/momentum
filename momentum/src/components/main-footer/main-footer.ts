4f485c7392da7940c5e3543b4e29992c72ebda3e
PANTORY <pantory@gmail.com>
2022-03-19 18:17:49 +0300

    refactor: minor markup & styles fixes

--- momentum/src/components/main-footer/main-footer.ts
+++ momentum/src/components/main-footer/main-footer.ts
@@ -2,13 +2,11 @@ export default class MainFooter {
   public static render(): string {
     return `
       <footer class="section main-footer" id="section-footer">
-        <div class="main-footer__left">
-          <a class="main-footer__link" href="https://github.com/viGITory" target="_blank" rel="noopener noreferrer" title="“This is the way..” (The Mandalorian)">viGITory</a>
-          <time datetime="2021">2021</time>
+        <a class="main-footer__link" href="https://github.com/viGITory" target="_blank" rel="noopener noreferrer" title="“This is the way..” (The Mandalorian)">viGITory</a>
+        <div>
+          <time datetime="2021">2021 /</time>
+          <a class="main-footer__link" href="https://rs.school/js" target="_blank" rel="noopener noreferrer">RSSchool</a>
         </div>
-        <a class="main-footer__logo" href="https://rs.school/js" target="_blank" rel="noopener noreferrer">
-          <span class="visually-hidden">Rsschool</span>
-        </a>
       </footer>
     `;
   }
